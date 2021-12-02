bc7ee7109e025b68f8124eed70ca391f4941b127
sarahbejerano <bejeranosarah@gmail.com>
2021-12-02 02:09:07 +0000

    dashboard page

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -3,8 +3,10 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./pages/home";
-import { Demo } from "./pages/demo";
-import { Single } from "./pages/single";
+import { Login } from "./pages/login";
+import { Register } from "./pages/register";
+import { Dashboard } from "./pages/dashboard";
+
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -25,12 +27,18 @@ const Layout = () => {
 						<Route exact path="/">
 							<Home />
 						</Route>
-						<Route exact path="/demo">
-							<Demo />
+						<Route exact path="/login">
+							<Login />
 						</Route>
-						<Route exact path="/single/:theid">
-							<Single />
+
+						<Route exact path="/register">
+							<Register />
 						</Route>
+
+						<Route exact path="/dashboard">
+							<Dashboard />
+						</Route>
+
 						<Route>
 							<h1>Not found!</h1>
 						</Route>
---
+++ src/front/js/pages/login.js
@@ -0,0 +1,95 @@
+import React, { useEffect } from "react";
+import { useHistory } from "react-router";
+import create from "zustand";
+
+export const useAuth = create(set => ({
+	error: false,
+	success: false,
+	token: null,
+
+	login: async (email, password) => {
+		const response = await fetch(process.env.BACKEND_URL + "/api/login", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({ email, password })
+		});
+
+		if (response.status === 204) {
+			set({ success: true });
+		} else {
+			set({ error: true });
+		}
+	},
+
+	login: async (email, password) => {
+		const response = await fetch(process.env.BACKEND_URL + "/api/login", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({ email, password })
+		});
+
+		if (response.status === 200) {
+			const payload = await response.json();
+			set({ token: payload.token });
+		} else {
+			set({ error: true });
+		}
+	}
+}));
+
+export const Login = () => {
+	const [email, setEmail] = React.useState("");
+	const [password, setPassword] = React.useState("");
+	const history = useHistory();
+	const login = useAuth();
+
+	useEffect(
+		() => {
+			if (login.token) {
+				history.push("/dashboard");
+			}
+		},
+		[login.token]
+	);
+	return (
+		<form>
+			{login.error && <div className="alert alert-danger">Error at login</div>}
+			<div className="mb-3">
+				<label htmlFor="exampleInputEmail1" className="form-label">
+					Email address
+				</label>
+				<input
+					type="email"
+					className="form-control"
+					id="exampleInputEmail1"
+					aria-describedby="emailHelp"
+					value={email}
+					onChange={ev => setEmail(ev.target.value)}
+				/>
+				<div id="emailHelp" className="form-text">
+					We never share your email with anyone else.
+				</div>
+			</div>
+			<div className="mb-3">
+				<label htmlFor="exampleInputPassword1" className="form-label">
+					Password
+				</label>
+				<input
+					type="password"
+					className="form-control"
+					id="exampleInputPassword1"
+					value={password}
+					onChange={ev => setPassword(ev.target.value)}
+				/>
+			</div>
+
+			<button type="button" className="btn btn-primary" onClick={() => login.login(email, password)}>
+				Login
+			</button>
+		</form>
+	);
+};
